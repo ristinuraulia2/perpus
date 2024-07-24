187468e61b7faaf06cd632cc80408325dc812585
ristinuraulia2 <144652936+ristinuraulia2@users.noreply.github.com>
2024-07-24 11:02:53 +0700

    Add files via upload

---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <form @submit.prevent="getPengunjung">
+          </form>
+        </div>
+        <div class="my-3 text-muted">menampilkan {{ visitors.length }} dari {{ banyak }}</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>NO</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+            <tbody>
+              <tr v-for="(visitor,i) in visitors" :key="i">
+                <td>{{ i+1 }}.</td>
+                <td>{{ visitor.nama }}</td>
+                <td>{{ visitor.keanggotaan.nama }}</td>
+                <td>{{ visitor.tanggal }}, {{ visitor.waktu }}</td>
+                <td>{{ visitor.keperluan.nama }}</td>
+                </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="d-flek justify-content-end">
+    <nuxt-link to="/" class=" btn btn-danger btn-lg rounded-4 px-4 ms-auto">KEMBALI</nuxt-link>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "PERPUS DIGITAL",
+  meta: [
+  {
+    name: "description",
+    content: "Halaman riwayat kunjungan",
+  },
+],
+});
+const supabase = useSupabaseClient();
+
+const keyword = ref([]);
+
+const visitors = ref([]);
+
+const banyak = ref([]);
+
+
+const getPengunjung = async () => {
+  const { data, error } = await supabase.from("pengunjung").select(`*,keanggotaan(*),keperluan(*)`).ilike("nama", `%${keyword.value}%`).order("id",{ascending: false});
+  if(data) visitors.value = data;
+};
+
+const banyakPengunjung = async () => {
+  const {data, count } = await supabase.from("pengunjung").select(`*`, {count: "exact"});
+  if (data) banyak.value = count;
+};
+
+onMounted(() => {
+  getPengunjung ()
+});
+</script>
+
+<style scoped>
+.btn-light {
+    background-color: #5fd8fe !important;
+    box-shadow: 1px 1px 10px #5fd8fe !important;
+}
+</style>
